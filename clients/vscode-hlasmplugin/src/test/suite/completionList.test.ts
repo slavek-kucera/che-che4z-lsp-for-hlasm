dda870df77c325d81ffa54939f7ab75271432749
slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
2023-01-20 17:39:45 +0100

    no clue

--- clients/vscode-hlasmplugin/src/test/suite/completionList.test.ts
+++ clients/vscode-hlasmplugin/src/test/suite/completionList.test.ts
@@ -18,12 +18,11 @@ import * as helper from './testHelper';
 
 suite('Completion List Test Suite', () => {
     const workspace_file = 'open';
-    let editor: vscode.TextEditor;
 
     suiteSetup(async function () {
         this.timeout(10000);
 
-        editor = (await helper.showDocument(workspace_file)).editor;
+        await helper.showDocument(workspace_file);
         vscode.window.onDidChangeVisibleTextEditors(e => console.log('onDidChangeVisibleTextEditors', e));
         vscode.window.onDidChangeActiveTextEditor(e => console.log('onDidChangeActiveTextEditor', e));
     });
@@ -35,10 +34,11 @@ suite('Completion List Test Suite', () => {
     // test completion list for instructions
     test('Completion List Instructions test', async () => {
         const diags = helper.waitForDiagnostics(workspace_file);
+        const { editor, document } = await helper.showDocument(workspace_file);
         const movePosition = await helper.insertString(editor, new vscode.Position(7, 1), 'L');
         await diags;
 
-        const completionList: vscode.CompletionList = await vscode.commands.executeCommand('vscode.executeCompletionItemProvider', editor.document.uri, movePosition);
+        const completionList: vscode.CompletionList = await vscode.commands.executeCommand('vscode.executeCompletionItemProvider', document.uri, movePosition);
 
         const result = completionList.items.filter(complItem => complItem.label.toString().startsWith('L'));
         assert.strictEqual(result.length, 343, 'Wrong number of suggestion result.');
@@ -49,6 +49,7 @@ suite('Completion List Test Suite', () => {
         // add '&' to simulate start of a variable symbol
         let movePosition: vscode.Position;
         const diags = helper.waitForDiagnostics(workspace_file);
+        const { editor, document } = await helper.showDocument(workspace_file);
         try {
             movePosition = await helper.insertString(editor, new vscode.Position(8, 0), '&');
         } catch (e) {
@@ -59,7 +60,7 @@ suite('Completion List Test Suite', () => {
         }
         await diags;
 
-        const completionList: vscode.CompletionList = await vscode.commands.executeCommand('vscode.executeCompletionItemProvider', editor.document.uri, movePosition);
+        const completionList: vscode.CompletionList = await vscode.commands.executeCommand('vscode.executeCompletionItemProvider', document.uri, movePosition);
 
         const labels = completionList.items.map(x => x.label.toString());
         assert.deepStrictEqual(labels, ['&VAR', '&VAR2'], 'Wrong suggestion result.');
