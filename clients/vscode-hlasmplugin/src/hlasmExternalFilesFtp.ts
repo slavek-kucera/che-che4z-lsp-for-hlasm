1f23ad45060bedfbcb00317ecb930243a48ccf01
Slavomir Kucera <slavomir.kucera@broadcom.com>
2023-04-19 18:20:32 +0200

    primitive client reuse

--- clients/vscode-hlasmplugin/src/hlasmExternalFilesFtp.ts
+++ clients/vscode-hlasmplugin/src/hlasmExternalFilesFtp.ts
@@ -19,6 +19,7 @@ import { ConnectionInfo, connectionSecurityLevel, gatherConnectionInfo, getLastR
 import { Writable } from 'stream';
 import { convertBuffer } from './conversions';
 import { TextDecoder } from 'util';
+import { cancelMessage } from './uiUtils';
 
 class StringWritable extends Writable {
     private chunks: Buffer[] = [];
@@ -53,51 +54,105 @@ const checkedCommand = async (client: ftp.Client, command: string): Promise<stri
 
 export class HLASMExternalFilesFtp implements ExternalFilesClient {
     private connInfo_: ConnectionInfo = null;
+    private suspended = false;
+
+    private client_: ftp.Client = null;
+    private client_timeout: ReturnType<typeof setTimeout> = null;
+
+    private async_lock: Promise<void> = null;
+
+    private static unused_timeout = 30000;
 
     constructor(private context: vscode.ExtensionContext) { }
 
     private async getConnInfo() {
-        if (this.connInfo_)
+        while (this.async_lock)
+            await this.async_lock;
+
+        let resolve: () => void;
+        this.async_lock = new Promise((r) => { resolve = r; })
+
+        try {
+            if (this.suspended)
+                return null;
+
+            if (this.connInfo_)
+                return this.connInfo_;
+
+            const last = getLastRunConfig(this.context);
+            this.connInfo_ = await gatherConnectionInfo(last);
+            await updateLastRunConfig(this.context, { host: this.connInfo_.host, user: this.connInfo_.user, jobcard: last.jobcard });
+
             return this.connInfo_;
+        }
+        catch (e) {
+            if (e.message === cancelMessage)
+                return null;
+            throw e;
+        }
+        finally {
+            resolve();
+            this.async_lock = null;
+        }
+    }
 
-        const last = getLastRunConfig(this.context);
-        this.connInfo_ = await gatherConnectionInfo(last);
-        await updateLastRunConfig(this.context, { host: this.connInfo_.host, user: this.connInfo_.user, jobcard: last.jobcard });
-
-        return this.connInfo_;
+    private cleanupSharedClient() {
+        this.client_.close();
+        this.client_ = null;
     }
 
     private async getConnectedClient() {
-        const connection = await this.getConnInfo();
-        const client = new ftp.Client();
-
-        try {
-            await client.access({
-                host: connection.host,
-                user: connection.user,
-                password: connection.password,
-                port: connection.port,
-                secure: connection.securityLevel !== connectionSecurityLevel.unsecure,
-                secureOptions: connection.securityLevel === connectionSecurityLevel.unsecure ? undefined : { rejectUnauthorized: connection.securityLevel !== connectionSecurityLevel.rejectUnauthorized }
-            });
-
-            client.parseList = (rawList: string): ftp.FileInfo[] => {
-                return rawList.split(/\r?\n/).slice(1).filter(x => !/^\s*$/.test(x)).map(value => new ftp.FileInfo(value.trim()));
-            };
-
-            return client;
+        if (this.client_) {
+            clearTimeout(this.client_timeout);
+            const client = this.client_;
+            this.client_ = null;
+            try {
+                //await client.pwd(); // check if alive
+                return client;
+            }
+            catch (e) {
+                client.close();
+            }
         }
-        catch (e) {
-            this.connInfo_ = null;
-            client.close();
-            throw e;
+
+        while (true) {
+            const client = new ftp.Client();
+            try {
+                const connection = await this.getConnInfo();
+
+                if (!connection) {
+                    this.suspended = true;
+                    return null;
+                }
+
+                await client.access({
+                    host: connection.host,
+                    user: connection.user,
+                    password: connection.password,
+                    port: connection.port,
+                    secure: connection.securityLevel !== connectionSecurityLevel.unsecure,
+                    secureOptions: connection.securityLevel === connectionSecurityLevel.unsecure ? undefined : { rejectUnauthorized: connection.securityLevel !== connectionSecurityLevel.rejectUnauthorized }
+                });
+
+                client.parseList = (rawList: string): ftp.FileInfo[] => {
+                    return rawList.split(/\r?\n/).slice(1).filter(x => !/^\s*$/.test(x)).map(value => new ftp.FileInfo(value.trim()));
+                };
+
+                return client;
+            }
+            catch (e) {
+                this.connInfo_ = null;
+                client.close();
+
+                vscode.window.showErrorMessage(e.message);
+            }
         }
     }
 
-    async listMembers(dataset: string): Promise<string[] | null | Error> {
-        const client = await this.getConnectedClient();
+    async listMembersImpl(client: ftp.Client, dataset: string): Promise<string[] | null | Error> {
         try {
             checkResponse(await client.cd(`'${dataset}'`));
+            await checkedCommand(client, 'TYPE A');
             const list = await client.list();
             return list.map(x => x.name);
         }
@@ -106,14 +161,16 @@ export class HLASMExternalFilesFtp implements ExternalFilesClient {
                 return null;
             return e;
         }
-        finally {
-            client.close();
-        }
     }
-    async readMember(dataset: string, member: string): Promise<string | null | Error> {
-        let client: ftp.Client = null;
+
+    async listMembers(dataset: string): Promise<string[] | null | Error> {
+        const client = await this.getConnectedClient();
+        if (!client) return null;
+        return this.listMembersImpl(client, dataset).finally(() => { this.requestEndHandle(client) });
+    }
+
+    async readMemberImpl(client: ftp.Client, dataset: string, member: string): Promise<string | null | Error> {
         try {
-            client = await this.getConnectedClient()
             const buffer = new StringWritable();
             buffer.on('error', err => { throw err });
 
@@ -127,9 +184,19 @@ export class HLASMExternalFilesFtp implements ExternalFilesClient {
                 return null;
             return e;
         }
-        finally {
-            //    if (client)
-            //        client.close();
+    }
+    async readMember(dataset: string, member: string): Promise<string | null | Error> {
+        const client = await this.getConnectedClient();
+        if (!client) return null;
+        return this.readMemberImpl(client, dataset, member).finally(() => { this.requestEndHandle(client); });
+    }
+
+    private requestEndHandle(client: ftp.Client) {
+        if (this.client_)
+            client.close();
+        else {
+            this.client_timeout = setTimeout(() => this.cleanupSharedClient(), HLASMExternalFilesFtp.unused_timeout);
+            this.client_ = client;
         }
     }
 
