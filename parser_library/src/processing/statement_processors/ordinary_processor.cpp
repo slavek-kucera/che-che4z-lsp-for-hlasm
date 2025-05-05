bff09dd08af9b0e7d9e9598295c689b6ca868dc4
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-05-05 13:16:56 +0200

    temp fix

--- parser_library/src/processing/statement_processors/ordinary_processor.cpp
+++ parser_library/src/processing/statement_processors/ordinary_processor.cpp
@@ -73,7 +73,10 @@ std::optional<processing_status> ordinary_processor::get_processing_status(
     if (auto status = get_instruction_processing_status(id, hlasm_ctx); status.has_value())
         return *status;
 
-    auto found = branch_provider_.request_external_processing(id.to_string(), processing_kind::MACRO, {});
+    auto name = id.to_string();
+    if (name.ends_with(":MAC"))
+        name.erase(name.size() - 4);
+    auto found = branch_provider_.request_external_processing(std::move(name), processing_kind::MACRO, {});
     if (!found.has_value())
         return std::nullopt;
 
