aadbef39a18030d27f8805e2e9d09598730ccc30
slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
2025-12-18 10:12:15 +0100

    Update prereq.darwin_x64.sh

--- scripts/prereq.darwin_x64.sh
+++ scripts/prereq.darwin_x64.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-brew install ninja llvm@18
-echo "LLVM_PATH=$(brew --prefix llvm@18)" >> $GITHUB_ENV
+brew install ninja llvm@21
+echo "LLVM_PATH=$(brew --prefix llvm@21)" >> $GITHUB_ENV
